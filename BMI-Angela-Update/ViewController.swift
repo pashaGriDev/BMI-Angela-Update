8fe02e93c63a8207719a3de55f0761804b256093
Pavel Gri <zlibabaika@gmail.com>
2023-01-31 03:49:01 +0400

    Initial Commit

---
+++ BMI-Angela-Update/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Angela-Update
+//
+//  Created by Павел Грицков on 31.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
